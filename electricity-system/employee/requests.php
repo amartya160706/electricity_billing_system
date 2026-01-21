ce44f73bfa3dc0f0b209a944d86816d2bc96e3f2
amartya160706 <amartyaleetcode16@gmail.com>
2026-01-22 02:40:52 +0530

    Delete electricity-system directory

--- electricity-system/employee/requests.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'employee') {
-    header('Location: ../index.php');
-    exit;
-}
-header('Location: bills.php');
-exit;
-
